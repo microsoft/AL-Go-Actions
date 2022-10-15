e05e6a3d5e3a246719fef09e4501ff82fb18d59f
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2022-10-15 06:08:10 +0200

    Update AL-Go-Helper.ps1

--- AL-Go-Helper.ps1
+++ AL-Go-Helper.ps1
@@ -246,12 +246,7 @@ function DownloadAndImportBcContainerHelper {
         }
         else {
             Write-Host "Downloading BcContainerHelper $BcContainerHelperVersion version"
-            try {
-                $webclient.DownloadFile("https://bccontainerhelper.azureedge.net/public/$($BcContainerHelperVersion).zip", "$tempName.zip")
-            }
-            catch {
-                $webclient.DownloadFile("https://bccontainerhelper.blob.core.windows.net/public/$($BcContainerHelperVersion).zip", "$tempName.zip")        
-            }
+            $webclient.DownloadFile("https://bccontainerhelper.blob.core.windows.net/public/$($BcContainerHelperVersion).zip", "$tempName.zip")        
         }
         Expand-7zipArchive -Path "$tempName.zip" -DestinationPath $tempName
         $BcContainerHelperPath = (Get-Item -Path (Join-Path $tempName "*\BcContainerHelper.ps1")).FullName
