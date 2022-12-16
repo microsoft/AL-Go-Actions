1e2fcf9e353e29a77f9b73fb27dd29d589aa5378
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2022-12-16 14:11:09 +0100

    Hotfix rate limit

--- CheckForUpdates/CheckForUpdates.ps1
+++ CheckForUpdates/CheckForUpdates.ps1
@@ -74,34 +74,27 @@ try {
     }
 
     if ($templateUrl -ne "") {
-        try {
-            $templateUrl = $templateUrl -replace "https://www.github.com/","$ENV:GITHUB_API_URL/repos/" -replace "https://github.com/","$ENV:GITHUB_API_URL/repos/"
-            Write-Host "Api url $templateUrl"
-            $templateInfo = Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri $templateUrl | ConvertFrom-Json
-        }
-        catch {
-            throw "Could not retrieve the template repository. Error: $($_.Exception.Message)"
-        }
+        $archiveUrl = "$($templateUrl -replace "https://www.github.com/","$ENV:GITHUB_API_URL/repos/" -replace "https://github.com/","$ENV:GITHUB_API_URL/repos/")/zipball/$templateBranch"
     }
     else {
         Write-Host "Api url $($ENV:GITHUB_API_URL)/repos/$($ENV:GITHUB_REPOSITORY)"
-        $repoInfo = Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri "$($ENV:GITHUB_API_URL)/repos/$($ENV:GITHUB_REPOSITORY)" | ConvertFrom-Json
+        $repoInfo = InvokeWebRequest -Headers $headers -Uri "$($ENV:GITHUB_API_URL)/repos/$($ENV:GITHUB_REPOSITORY)" -retry | ConvertFrom-Json
         if (!($repoInfo.PSObject.Properties.Name -eq "template_repository")) {
             OutputWarning -message "This repository wasn't built on a template repository, or the template repository is deleted. You must specify a template repository in the AL-Go settings file."
             exit
         }
-
         $templateInfo = $repoInfo.template_repository
+        $templateUrl = $templateInfo.html_url
+        $archiveUrl = $templateInfo.archive_url.Replace('{archive_format}','zipball').replace('{/ref}',"/$templateBranch")
     }
 
-    $templateUrl = $templateInfo.html_url
     Write-Host "Using template from $templateUrl@$templateBranch"
+    Write-Host "ArchiveUrl $archiveUrl"
 
     $headers = @{             
         "Accept" = "application/vnd.github.baptiste-preview+json"
     }
-    $archiveUrl = $templateInfo.archive_url.Replace('{archive_format}','zipball').replace('{/ref}',"/$templateBranch")
-    $tempName = Join-Path $env:TEMP ([Guid]::NewGuid().ToString())
+    $tempName = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid().ToString())
     Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri $archiveUrl -OutFile "$tempName.zip"
     Expand-7zipArchive -Path "$tempName.zip" -DestinationPath $tempName
     Remove-Item -Path "$tempName.zip"
