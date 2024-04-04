5f6a994563508e88ab3596e124cbdeb795bbdadd
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2024-04-04 11:19:41 +0200

    v3.1 - Update AL-Go-Helper.ps1 (#49)

--- AL-Go-Helper.ps1
+++ AL-Go-Helper.ps1
@@ -293,14 +293,13 @@ function DownloadAndImportBcContainerHelper {
                 $webclient.DownloadFile("https://bccontainerhelper.blob.core.windows.net/public/$($bcContainerHelperVersion).zip", "$tempName.zip")
             }
         }
-        elseif ($bcContainerHelperVersion -eq "preview" -or $bcContainerHelperVersion -eq "dev") {
-            # For backwards compatibility, use preview when dev is specified
-            Write-Host "Downloading BcContainerHelper $bcContainerHelperVersion version from Blob Storage"
-            $webclient.DownloadFile("https://bccontainerhelper.blob.core.windows.net/public/preview.zip", "$tempName.zip")
-        }
         else {
-            Write-Host "Downloading BcContainerHelper $bcContainerHelperVersion version from CDN"
-            $webclient.DownloadFile("https://bccontainerhelper.azureedge.net/public/$($bcContainerHelperVersion).zip", "$tempName.zip")
+            if ($bcContainerHelperVersion -eq "dev") {
+                # For backwards compatibility, use preview when dev is specified
+                $bcContainerHelperVersion = 'preview'
+            }
+            Write-Host "Downloading BcContainerHelper $bcContainerHelperVersion version from Blob Storage"
+            $webclient.DownloadFile("https://bccontainerhelper.blob.core.windows.net/public/$($bcContainerHelperVersion).zip", "$tempName.zip")
         }
         Expand-7zipArchive -Path "$tempName.zip" -DestinationPath $tempName
         $BcContainerHelperPath = (Get-Item -Path (Join-Path $tempName "*\BcContainerHelper.ps1")).FullName
